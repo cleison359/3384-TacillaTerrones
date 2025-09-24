e00d47a039e4e183e9c3a916b0539ec762c21eb4
cleison359 <n00465633@upn.pe>
2025-09-24 12:13:11 -0500

    agregando ejercicio 1 en csharp

---
+++ estructuras de control basicas/repetitivas/for/csharp/ejercicio 1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Número: " + i);
+        }
+    }
+}
