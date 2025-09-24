c1dd63e0fd230adb009037e79a4196478ef73c88
cleison359 <n00465633@upn.pe>
2025-09-24 11:02:27 -0500

    agregando ejercicio 1 en csharp

---
+++ estructuras de control basicas/secuenciales/csharp/ejercicio 1.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int num1 = 5;
+        int num2 = 10;
+        int suma = num1 + num2;
+
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
