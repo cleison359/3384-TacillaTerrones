901a8bb50da913947679161df02ead491e739860
cleison359 <n00465633@upn.pe>
2025-09-24 11:03:14 -0500

    agregando ejercicio 2 en csharp

---
+++ estructuras de control basicas/secuenciales/csharp/ejercicio 2.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        double baseTriangulo = 8;
+        double altura = 5;
+        double area = (baseTriangulo * altura) / 2;
+
+        Console.WriteLine("El área del triángulo es: " + area);
+    }
+}
