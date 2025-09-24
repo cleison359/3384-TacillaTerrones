68231946ff2f681a7f266ed24501d091a3e58796
cleison359 <n00465633@upn.pe>
2025-09-24 11:18:19 -0500

    agregando ejercicio 1 en csharp

---
+++ estructuras de control basicas/condicionales/csharp/ejercicio 1.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int numero = 15;
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El número es positivo.");
+        }
+    }
+}
