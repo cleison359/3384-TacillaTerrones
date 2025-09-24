e8cfc7201a03716fdd5ffcf67b2c2851af1a5c84
cleison359 <n00465633@upn.pe>
2025-09-24 11:35:14 -0500

    agregando ejercicio 2 en csharp

---
+++ estructuras de control basicas/condicionales/dobles/csharp/ejercicio 2.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        double nota = 5.2;
+
+        if (nota >= 6.0)
+        {
+            Console.WriteLine("El estudiante aprobó.");
+        }
+        else
+        {
+            Console.WriteLine("El estudiante reprobó.");
+        }
+    }
+}
