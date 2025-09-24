61a16766346ab2596a0c73d8099acd7fc65d19ea
cleison359 <n00465633@upn.pe>
2025-09-24 11:52:55 -0500

    agregando ejemplo 2 en python

---
+++ estructuras de control basicas/condicionales/anidadas/python/ejemplo 2.py
@@ -0,0 +1,13 @@
+nota = 8.7
+
+if nota >= 6.0:
+    print("El estudiante aprobó.")
+
+    if nota >= 9.0:
+        print("¡Excelente!")
+    elif nota >= 7.0:
+        print("Buen trabajo.")
+    else:
+        print("Aprobado, pero necesita mejorar.")
+else:
+    print("El estudiante reprobó.")
