be6125f727865e420a701bc4479ea8ef5ba59bf0
cleison359 <n00465633@upn.pe>
2025-09-24 12:02:42 -0500

    agregando ejercicio 2 en python

---
+++ estructuras de control basicas/condicionales/multiples/python/ejercicio 2.py
@@ -0,0 +1,10 @@
+opcion = 3
+
+if opcion == 1:
+    print("Has seleccionado la opción 1")
+elif opcion == 2:
+    print("Has seleccionado la opción 2")
+elif opcion == 3:
+    print("Has seleccionado la opción 3")
+else:
+    print("Opción no válida")
