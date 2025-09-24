a915fd0b1fe84ad88b666bfccc0228a366824e39
cleison359 <n00465633@upn.pe>
2025-09-24 11:52:23 -0500

    agregando ejemplo 1 en python

---
+++ estructuras de control basicas/condicionales/anidadas/python/ejemplo 1.py
@@ -0,0 +1,9 @@
+numero = -3
+
+if numero != 0:
+    if numero > 0:
+        print("El número es positivo.")
+    else:
+        print("El número es negativo.")
+else:
+    print("El número es cero.")
