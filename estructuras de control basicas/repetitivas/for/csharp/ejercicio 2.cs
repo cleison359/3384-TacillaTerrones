9ad7842db8e2d59c7d29f35ab5870089397fec74
cleison359 <n00465633@upn.pe>
2025-09-24 12:14:11 -0500

    agregando ejercicio 2 en csharp

---
+++ estructuras de control basicas/repetitivas/for/csharp/ejercicio 2.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int suma = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            suma += i;
+        }
+
+        Console.WriteLine("La suma de los primeros 10 números es: " + suma);
+    }
+}
