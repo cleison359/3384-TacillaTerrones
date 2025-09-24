dcafef5c785fd8283dfab2ff292494613719c118
cleison359 <n00465633@upn.pe>
2025-09-24 11:11:03 -0500

    agregando ejercicio 2 en python

---
+++ estructuras de control basicas/secuenciales/python/ejercicio 2.py
@@ -0,0 +1,8 @@
+# Declarar temperatura en Celsius
+celsius = 25
+
+# Convertir a Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Mostrar resultado
+print("La temperatura en Fahrenheit es:", fahrenheit)
