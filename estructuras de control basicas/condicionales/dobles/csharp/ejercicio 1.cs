ceb1fba6c16e7c3bf680c85ee073525a2856609f
cleison359 <n00465633@upn.pe>
2025-09-24 11:34:33 -0500

    agregando ejercicio 1 en csharp

---
+++ estructuras de control basicas/condicionales/dobles/csharp/ejercicio 1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int numero = 7;
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar.");
+        }
+    }
+}
